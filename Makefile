b9efe80c509a5218c8eae7af5e1a29584327dce6
Dav-11 <48259205+Dav-11@users.noreply.github.com>
2025-05-22 23:27:54 +0200

    initial commit

---
+++ Makefile
@@ -0,0 +1,4 @@
+
+.PHONY: build
+build:
+	go build -o bin/cache_sim_go .
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package main
+
+import "Dav-11/cache_sim_go/cmd"
+
+func main() {
+	cmd.Execute()
+}
